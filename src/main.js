fbd0149fccf4df72dfc68f76685c9223288e57b6
camilajl <camilajimenezlopez@gmail.com>
2022-07-26 18:25:15 -0500

    [ADD] Third Commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,24 @@ async function getTrendingMoviesPreview(){
     const data = await res.json();
 
     const movies = data.results;
-    console.log({data, results});
+    console.log({data, movies});
+    movies.forEach(movie => {
+
+        const getTrendingMoviesPreviewContainer = document.querySelector('#trendingPreview-header .trendingPreview-movieList')
+
+        const movieContainer = document.createElement('div')
+        movieContainer.classList.add('movie-container');
+
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://www.themoviedb.org/t/p/w440/' + movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        getTrendingMoviesPreviewContainer.appendChild(movieContainer);
+
+        
+    });
 
 }
 
