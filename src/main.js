e0df9bfa6b88510fcf1561ae7f5f9625f4b76ef7
camilajl <camilajimenezlopez@gmail.com>
2022-07-26 17:33:22 -0500

    [ADD] Second Commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+async function getTrendingMoviesPreview(){
+
+    const res = await fetch ('https://api.themoviedb.org/3/trending/movie/day?api_key='+ API_KEY)
+    const data = await res.json();
+
+    const movies = data.results;
+    console.log({data, results});
+
+}
+
+getTrendingMoviesPreview()
